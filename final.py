4e548e1eeb576af9d2ef4d95885b88d3919e970f
walter <walter@localhost.localdomain>
2020-03-19 23:54:49 +0530

    final commit

--- final.py
+++ final.py
@@ -1,6 +1,12 @@
 import scanner
 
-target_url = "http://192.168.1.6/mutillidae/"
+# target_url = "http://192.168.1.6/mutillidae/"
 
-vuln_scanner = scanner.Scanner(target_url)
-vuln_scanner.crawl(target_url)
+target_url = "http://192.168.1.6/dvwa/"
+links_to_ignore = ["http://192.168.1.6/dvwa/logout.php"]
+data_dict = {"username" : "admin", "password" : "password", "Login": "submit"}
+
+vuln_scanner = scanner.Scanner(target_url, links_to_ignore)
+vuln_scanner.session.post("http://192.168.1.6/dvwa/login.php", data=data_dict)
+vuln_scanner.crawl()
+vuln_scanner.run_scanner()
